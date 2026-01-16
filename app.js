30b585a513b280787c38dd73749911269a4bdb89
Sadiya-Sheikh-2026 <ss3377a@nulondon.ac.uk>
2026-01-16 02:17:04 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// basic script for a 'Generate password' button which has the id of 'generate' in the html script, the process is triggered by a click
+
+const generateButton = document.getElementById('generate'); 
+
+generateButton.addEventListener('click')
